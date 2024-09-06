c129e33372a8bac1940e466824137f85f4937bcb
uPos3odon08 <a.skarmark.jakobsen@gmail.com>
2024-09-06 12:51:02 +0200

    fixed the global variable issue

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import os
 from time import sleep
 
-
 def gameOver():
-    print ("Wow yea, you really died")
-    os.system('pause') 
+    print("Wow yea, you really died")
+    input("Press Enter to continue...") 
     os.system('clear||cls')
-    print ("Game over")
-    decision = input ("Do you want to play again? (Yes/No) > ")
-    if decision in ['yes']:
+    print("Game over")
+    decision = input("Do you want to play again? (Yes/No) > ")
+    if decision.lower() == 'yes':
         beginning()
     else: 
         quit()
@@ -18,47 +17,49 @@ def beginning():
     karma = 1000
 
     os.system('clear||cls')
-    print ("You come back to your senses and continue your speech")
+    print("You come back to your senses and continue your speech")
     sleep(5)
-    print ("Suddenly you see a weird glare in the distance and your bodyguard yelling")
-    sleep (3)
-    print ("GET DOWN!")
+    print("Suddenly you see a weird glare in the distance and your bodyguard yelling")
+    sleep(3)
+    print("GET DOWN!")
     toDodgeOrNoToDodge()
 
 def toDodgeOrNoToDodge():
     sleep(5)
-    print ("Do you get down?")
-    answer = input ("Yes/No > ")
+    print("Do you get down?")
+    answer = input("Yes/No > ")
 
-    if answer in ['no, No, NO, nO']:
+    if answer.lower() in ['no']:
         sleep(2)
-        print ("You suddenly hear a shot in the distance and it all goes black.")
-        sleep (7)
+        print("You suddenly hear a shot in the distance and it all goes black.")
+        sleep(7)
         os.system('clear||cls')
         gameOver()
         
     else:
         sleep(2)
-        print ("You dodged the bullet")
+        print("You dodged the bullet")
         sleep(2)
-        print ("but barely")
-        sleep (5)
-        os.system('pause') 
+        print("but barely")
+        sleep(5)
+        input("Press Enter to continue...") 
         theAftermath()
 
 def theAftermath():
-    print ("You get crowded by bodyguards using themselves as protection and making sure you get to the ground")
-    sleep(10)
-    print ("the bullet was millimeters away from hitting you")
-    os.system('pause') 
+    global karma  
+    os.system('clear||cls')
+    print("You get crowded by bodyguards using themselves as protection and making sure you get to the ground")
+    sleep(7)
+    print("the bullet was millimeters away from hitting you")
+    input("Press Enter to continue...") 
     os.system('clear||cls')
 
-    print ("The highest ranking member of the secret services present at the scene")
-    print ("tells you that they have a shot on the shooter, it is your call as to weather they shoot or arrest him")
+    print("The highest ranking member of the secret services present at the scene")
+    print("tells you that they have a shot on the shooter, it is your call as to whether they shoot or arrest him")
     sleep(14)
-    print ("i want you to ---- him")
-    answer = input ("Cuff/Shoot")
-    if answer in ['Shoot', 'shoot']:
+    print("i want you to ---- him")
+    answer = input("Cuff/Shoot > ")
+    if answer.lower() == 'shoot':
         karma -= 100
         theShot()
     else:
@@ -66,11 +67,13 @@ def theAftermath():
         theArrest() 
 
 def theArrest():
-    print ("arrest")
-    print (karma)
+    global karma 
+    print("arrest")
+    print(karma)
 
 def theShot():
-    print ("shoot")
-    print (karma)
+    global karma  
+    print("shoot")
+    print(karma)
 
 beginning()
